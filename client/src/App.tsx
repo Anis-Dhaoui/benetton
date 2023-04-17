d3001a4171d93a1e2fb57ae71bf662216bdae959
Anis Dhaoui <freeconcept6@gmail.com>
2023-04-17 13:19:42 +0100

    Adding .env file and redux folder structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import Main from './components/Main';
 
 function App() {
+
   return (
     <div className="App">
       <Main />
--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 function Main() {
+    console.log(process.env.REACT_APP_BASE_URL)
   return (
     <div>Main</div>
   )
