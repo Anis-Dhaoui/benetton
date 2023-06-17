46413d18fdb835d2435711f120af639cfe9d4932
Anis Dhaoui <freeconcept6@gmail.com>
2023-06-17 09:06:12 +0100

    Fixing some bugs! Let's go to update computers

--- client/src/components/computers/computer-form/Form.computers.tsx
+++ client/src/components/computers/computer-form/Form.computers.tsx
@@ -121,9 +121,10 @@ function ComputerForm() {
                       })
                     }
                     name="brandName"
+                    defaultValue={'DELL'}
                   >
                     <option disabled>Choisir la marque du PC</option>
-                    <option selected>DELL</option>
+                    <option>DELL</option>
                     <option>HP</option>
                     <option>TOSHIBA</option>
                     <option>ASUS</option>
@@ -174,11 +175,12 @@ function ComputerForm() {
                       })
                     }
                     name="os"
+                    defaultValue={'WIN-10'}
                   >
                     <option disabled>Choisir OS</option>
                     <option>WIN-XP</option>
                     <option>WIN-7</option>
-                    <option selected>WIN-10</option>
+                    <option>WIN-10</option>
                     <option>WIN-11</option>
                     <option>LINUX</option>
                   </select>
@@ -226,9 +228,10 @@ function ComputerForm() {
                       })
                     }
                     name="status"
+                    defaultValue={'Active'}
                   >
                     <option disabled>Choisir status</option>
-                    <option selected>Active</option>
+                    <option>Active</option>
                     <option>En panne</option>
                     <option>Inactive</option>
                   </select>
@@ -303,9 +306,10 @@ function ComputerForm() {
                       })
                     }
                     name="type"
+                    defaultValue={'Desktop'}
                   >
                     <option disabled>Choisir type</option>
-                    <option selected>Desktop</option>
+                    <option>Desktop</option>
                     <option>Laptop</option>
                   </select>
                 </FormGroup>
@@ -375,8 +379,9 @@ function ComputerForm() {
                       })
                     }
                     name="group"
+                    defaultValue={'Choisir groupe'}
                   >
-                    <option disabled selected>Choisir groupe</option>
+                    <option disabled>Choisir groupe</option>
                     <option>Programmation</option>
                     <option>Modelery</option>
                     <option>Qualité</option>
@@ -423,192 +428,3 @@ function ComputerForm() {
 }
 
 export default ComputerForm
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState } from 'react'
-// import Multiselect from 'multiselect-react-dropdown';
-// import { interestsList, INTERESTS } from './interestsList.entry';
-// import { useForm, SubmitHandler } from "react-hook-form";
-// import { Tooltip, OverlayTrigger } from 'react-bootstrap';
-// import { useAppDispatch, useAppSelector } from '../../state/store.state';
-// import { handleRegister } from '../../state/actions-creators/register.actions-creators';
-
-// /* eslint-disable @typescript-eslint/no-unused-vars */
-// function RegisterCmp() {
-//     const dispatch = useAppDispatch();
-//     const { loading, res, errMsg } = useAppSelector(state => state.register);
-//     let { register, handleSubmit, watch, formState: { errors } } = useForm<IRegisterReq>({ mode: 'all' });
-//     const onSubmit: SubmitHandler<IRegisterReq> = (data) => {
-//         data.interests = selectedItems;
-//         dispatch(handleRegister(data));
-//     }
-
-//     const [selectedItems, setNetDrivesList] = useState<INTERESTS[]>([]);
-//     // Reusable function to handle Error messages of all input fields
-//     const tooltipErrMsg = (errMsg: string | undefined) => {
-//         if (errMsg) {
-//             return (
-//                 <Tooltip> {errMsg} </Tooltip>
-//             )
-//         }
-//         return <></>
-//     }
-
-//     return (
-//         <form onSubmit={handleSubmit(onSubmit)} className="sign-up-form">
-//             <h2 className="title">Register</h2>
-//             <div className="row">
-//                 <OverlayTrigger show={true} overlay={tooltipErrMsg(errors.firstName?.message)} >
-//                     <div className={errors.firstName ? 'input-field col-md-5 offset-md-1 mr-1 col fail' : 'input-field col-md-5 offset-md-1 mr-1 col success'}>
-//                         <i className="fas fa-user"></i>
-//                         <input type="text" autoComplete="given-name" placeholder="First name"
-//                             {...register("firstName",
-//                                 {
-//                                     required: "Required field",
-//                                     minLength: {
-//                                         value: 3,
-//                                         message: "First name should have at least 3 caracters"
-//                                     },
-//                                     maxLength: {
-//                                         value: 10,
-//                                         message: "First name should have at most 10 caracters"
-//                                     }
-//                                 })
-//                             }
-//                             name='firstName'
-//                         />
-//                     </div>
-//                 </OverlayTrigger>
-
-//                 <OverlayTrigger show={true} overlay={tooltipErrMsg(errors.lastName?.message)}>
-//                     <div className={errors.lastName ? 'input-field col-md-5 col fail' : 'input-field col-md-5 col success'}>
-//                         <i className="fas fa-user"></i>
-//                         <input type="text" autoComplete="given-name" placeholder="First name"
-//                             {...register("lastName",
-//                                 {
-//                                     required: "Required field",
-//                                     minLength: {
-//                                         value: 3,
-//                                         message: "Last name should have at least 3 caracters"
-//                                     },
-//                                     maxLength: {
-//                                         value: 10,
-//                                         message: "Last name should have at most 10 caracters"
-//                                     }
-//                                 })
-//                             }
-//                             name='lastName'
-//                         />
-//                     </div>
-//                 </OverlayTrigger>
-//             </div>
-
-//             <OverlayTrigger show={true} placement='right' overlay={tooltipErrMsg(errors.email?.message)}>
-//                 <div className={errors.email ? 'input-field fail' : 'input-field success'}>
-//                     <i className="fas fa-envelope"></i>
-//                     <input type="email" autoComplete="email" placeholder="Email"
-//                         {...register("email",
-//                             {
-//                                 required: "Required field",
-//                                 pattern: {
-//                                     value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
-//                                     message: 'Email is not valid.'
-//                                 }
-//                             })
-//                         }
-//                         name="email"
-//                     />
-//                 </div>
-//             </OverlayTrigger>
-
-//             <OverlayTrigger show={true} placement='right' overlay={tooltipErrMsg(errors.password?.message)}>
-//                 <div className={errors.password ? 'input-field fail' : 'input-field success'}>
-//                     <i className="fas fa-lock"></i>
-//                     <input type="password" autoComplete='current-password' placeholder="Password" id="id_reg"
-//                         {...register("password",
-//                             {
-//                                 required: "Required field",
-//                                 pattern: {
-//                                     value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
-//                                     message: 'Password should be combination of [a-z, A-Z, 0-9] with at least 8 characters'
-//                                 }
-//                             })
-//                         }
-//                         name="password"
-//                     />
-//                     <i className="far fa-eye" id="toggleReg" style={{ cursor: 'pointer' }}></i>
-//                 </div>
-//             </OverlayTrigger>
-
-//             <OverlayTrigger show={true} placement='right' overlay={tooltipErrMsg(errors.interests?.message)}>
-//                 <div className={errors.interests ? 'interests-list fail' : 'interests-list success'}>
-//                     <i className="fas fa-list-check"></i>
-//                     <Multiselect
-//                         onRemove={(items: INTERESTS[]) => { setNetDrivesList([...items]) }}
-//                         onSelect={(items: INTERESTS[]) => { setNetDrivesList([...items]) }}
-//                         isObject={false}
-//                         options={interestsList}
-//                         showArrow
-//                         showCheckbox
-//                         hidePlaceholder
-//                         placeholder='Choose your interests'
-//                         avoidHighlightFirstOption={true}
-//                         {...register('interests',
-//                             {
-//                                 required: selectedItems.length < 3 ? 'Select at least 3 categories' : '',
-//                             }
-//                         )}
-//                     />
-//                 </div>
-//             </OverlayTrigger>
-
-//             <label className="check">
-//                 <input type="checkbox" />
-//                 <span className="checkmark"> I accept the <a href="terms.html">terms and services</a></span>
-//             </label>
-//             <button disabled={loading} type="submit" style={{ width: '180px' }} className="entry-btn solid">
-//                 {loading ? <i className="fas fa-spinner fa-spin"></i> : 'Create account'}
-//             </button>
-
-//             <p className="social-text">You can register with:</p>
-//             <div className="social-media">
-//                 <a href="/#" className="social-icon" aria-label="Register with Google">
-//                     <i className="fab fa-google"></i>
-//                 </a>
-//                 <a href="/#" className="social-icon" aria-label="Register with Discord">
-//                     <i className="fab fa-discord"></i>
-//                 </a>
-//                 <a href="/#" className="social-icon" aria-label="Register with Twitter">
-//                     <i className="fab fa-twitter"></i>
-//                 </a>
-//                 <a href="/#" className="social-icon">
-//                     <i className="fab fa-facebook-f" aria-label="Register with Facebook"></i>
-//                 </a>
-//             </div>
-//         </form>
-//     )
-// }
-
-// export default RegisterCmp
