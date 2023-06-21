d8f774da9b824de54b6fb84e89f171480b6e9237
Anis Dhaoui <freeconcept6@gmail.com>
2023-06-21 11:03:56 +0100

    Delete confirm alert

--- client/src/components/computers/computers-table/actionsButton/ActionsBtn.computers.tsx
+++ client/src/components/computers/computers-table/actionsButton/ActionsBtn.computers.tsx
@@ -1,25 +1,46 @@
-import React from 'react'
-import './actionsbtn.css'
+import React, { useState } from 'react'
+import './actionsbtn.scss'
 import { deleteComputer } from '../../../../state/actions-creators/computer.actions-creators';
 import { useAppDispatch, useAppSelector } from '../../../../state/store.state';
 
 
-function ActionsBtn({computer}: IComputer | any) {
-  const dispatch = useAppDispatch();
-    
-    const handleDelete = (pc: IComputer) =>{
+function ActionsBtn({ computer }: IComputer | any) {
+    const dispatch = useAppDispatch();
+    const [delConfAlert, setDelConfAlert] = useState(false)
+
+    const handleDelete = (pc: IComputer) => {
         dispatch(deleteComputer(pc._id))
     }
+
+    const handleDelConfAlert = () => {
+        setDelConfAlert(!delConfAlert);
+    }
+
     return (
         <>
-            <div id='edit-remove' className='row'>
-                <div className='col-6'>
-                    <i className="fa fa-edit"></i>
-                </div>
-                <div className='col-6'>
-                    <i className="fa fa-trash" onClick={() => handleDelete(computer)}></i>
-                </div>
-            </div>
+            {
+                delConfAlert ?
+                    <div id='edit-remove' className='row'>
+                        <div className='col-6'>
+                            <i className="fa fa-check" onClick={() => handleDelete(computer)}></i>
+                        </div>
+                        <div className='col-6'>
+                            <i className="fa fa-close" onClick={handleDelConfAlert}></i>
+                        </div>
+                    </div>
+                    :
+                    <div id='edit-remove' className='row'>
+                        <div className='col-6'>
+                            <i className="fa fa-edit"></i>
+                        </div>
+                        <div className='col-6'>
+                            <i className="fa fa-trash" onClick={handleDelConfAlert}></i>
+                        </div>
+                    </div>
+            }
+
+
+
         </>
 
     )
