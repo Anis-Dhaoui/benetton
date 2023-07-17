e98069748b37b861c54015d9ffc0c953d0603566
Anis Dhaoui <freeconcept6@gmail.com>
2023-07-17 12:27:53 +0100

    changing server IP with host IP

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,15 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   //Without the following line DTO will never work
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: ["http://192.168.213.128:5001", "http://localhost:5001"]
+  app.enableCors();
+  await app.listen(5000, '192.168.213.128', () =>{
+    Logger.log(`Server is Running at port: 5000, and host: 192.168.213.128`)
   });
-  await app.listen(5000);
 }
 bootstrap();
